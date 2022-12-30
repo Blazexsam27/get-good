38a065e1d94d1d7ff1e085ba488bb2589003fb1a
Sanju Raj Prasad <43962657+Blazexsam27@users.noreply.github.com>
2022-12-30 19:32:59 +0530

    Minor ui changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,12 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
-import { useProductContext } from "../context/productContext";
 
 function About() {
-  const myName = useProductContext();
   const data = {
-    title: `ABOUT ${myName}`,
+    title: `ABOUT GET-GOOD`,
     desc: `GET GOOD is an e-commerce website which provide the best products
     and deals to the end user. Built using some of the most popular technologies
-    to bring the user a simple but awesome experience.`,
+    to bring the user an amazing and awesome experience.`,
   };
   return <HeroSection data={data} />;
 }
